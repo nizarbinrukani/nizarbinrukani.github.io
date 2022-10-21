de1dd749712702ca31bc92a6b45780d545c66e63
Nizar Kurniawan <kurniawan.nizar.ploso@gmail.com>
2022-10-21 10:46:50 +0700

    tambah submit fuction js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbzbfivZ3pB022Qh-v05EESV7_ShtKK3EqlO2euaWeDJdm4i5J5oi38MomRXo_zNU3qZkA/exec";
 const form = document.forms["submit-to-google-sheet"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  btnKirim.classList.toggle("d-none");
+  btnLoading.classList.toggle("d-none");
+
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      btnKirim.classList.toggle("d-none");
+      btnLoading.classList.toggle("d-none");
+      myAlert.classList.toggle("d-none");
+
+      form.reset();
+
+      console.log("Success!", response);
+    })
     .catch((error) => console.error("Error!", error.message));
 });
